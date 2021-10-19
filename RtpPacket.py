7cc69a0055c0958c0035a9a4ba489e8c8d407e6a
Truc Nguyen <truccm2001@gmail.com>
2021-10-19 12:34:20 +0700

    Update encode in RtpPacket

--- RtpPacket.py
+++ RtpPacket.py
@@ -17,22 +17,23 @@ class RtpPacket:
 		header[0] = version << 6  							# 2 bits
 		header[0] = header[0] | padding << 5 				# 1 bit
 		header[0] = header[0] | extension << 4            	# 1 bit
-		header[0] = header[0] | cc              			# 4 bits
+		header[0] = header[0] | cc   						# 4 bits
+
 		header[1] = marker << 7               				# 1 bit
 		header[1] = header[1] | pt               			# 7 bits
 		
 		header[2] = seqnum >> 8                     		# 16 bits total, this is first 8
-		header[3] = seqnum                            		# second 8
+		header[3] = seqnum & 0xFF                           # second 8
 		
 		header[4] = (timestamp >> 24) & 0xFF                # 32 bit timestamp
 		header[5] = (timestamp >> 16) & 0xFF
 		header[6] = (timestamp >> 8) & 0xFF
 		header[7] = timestamp & 0xFF
 		
-		header[8] = ssrc >> 24                              # 32 bit ssrc
-		header[9] = ssrc >> 16 
-		header[10] = ssrc >> 8 
-		header[11] = ssrc 
+		header[8] = (ssrc >> 24) & 0xFF                      # 32 bit ssrc
+		header[9] = (ssrc >> 16) & 0xFF
+		header[10] = (ssrc >> 8) & 0xFF
+		header[11] = ssrc & 0xFF 
 		
 		self.header = header
         
